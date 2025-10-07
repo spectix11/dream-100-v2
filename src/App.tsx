e617ccdbec690e794eeafa5ef3d187e10266b20a
ContextCodeX <mani@growthcreators.ai>
2025-10-07 02:10:56 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-[#0f0f0f]">
+    <div className="min-h-screen bg-[#0f0f0f] flex">
       {/* Sidebar */}
       <Sidebar
         activeSection={activeSection}
@@ -39,7 +39,7 @@ function App() {
       />
 
       {/* Main Content */}
-      <main className="ml-64 flex flex-col min-h-screen">
+      <main className="flex-1 flex flex-col">
         {renderActiveSection()}
       </main>
 
